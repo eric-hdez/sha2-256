98994fe180a98f924477c663b0e83d3abeb6b147
Eric Hernandez <eric.hdez2001@gmail.com>
2022-08-07 23:29:55 -0700

    spacing was vscode issue with tabs instead of spaces

--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"os"
-	"time"
+    "crypto/sha256"
+    "fmt"
+    "os"
+    "time"
 
-	"./sha2"
+    "./sha2"
 )
 
 func main() {
-	msg, err := os.ReadFile("loremipsum.txt")
-	if err != nil {
-		panic(err)
-	}
+    msg, err := os.ReadFile("loremipsum.txt")
+    if err != nil {
+        panic(err)
+    }
 
-	message := []byte(msg)
+    message := []byte(msg)
 
-	myStart := time.Now()
-	myHash := sha2.Hash256(message)
-	myEnd := time.Now()
+    myStart := time.Now()
+    myHash := sha2.Hash256(message)
+    myEnd := time.Now()
 
-	myElapsed := myEnd.Sub(myStart)
-	fmt.Println("my sha256: ", myElapsed, "\n", myHash)
+    myElapsed := myEnd.Sub(myStart)
+    fmt.Println("my sha256: ", myElapsed, "\n", myHash)
 
-	stdStart := time.Now()
-	stdHash := sha256.Sum256(message)
-	stdEnd := time.Now()
+    stdStart := time.Now()
+    stdHash := sha256.Sum256(message)
+    stdEnd := time.Now()
 
-	stdElapsed := stdEnd.Sub(stdStart)
-	fmt.Println("std sha256: ", stdElapsed, "\n", stdHash)
+    stdElapsed := stdEnd.Sub(stdStart)
+    fmt.Println("std sha256: ", stdElapsed, "\n", stdHash)
 }
