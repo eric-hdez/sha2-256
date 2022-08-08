f5d1b3f679d989a4f9b3c432126a1e11b893082f
Eric Hernandez <eric.hdez2001@gmail.com>
2022-08-07 23:24:01 -0700

    spacing was vscode issue with tabs instead of spaces

--- sha2/sha2.go
+++ sha2/sha2.go
@@ -1,30 +1,30 @@
-    package sha2
+package sha2
 
-    import (
+import (
     "encoding/binary"
     "math/bits"
-    )
+)
 
-    const (
+const (
     blockSize int = 64
     byteSize  int = 8
     u64Bytes  int = 8
     u32Bytes  int = 4
-    )
+)
 
-    /*
-    hash values:
-        the first 32 bits of the fractional parts of the square roots of the first eight primes
-    */
-    var h = [8]uint32{
+/*
+  hash values:
+    the first 32 bits of the fractional parts of the square roots of the first eight primes
+*/
+var h = [8]uint32{
     0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
-    }
+}
 
-    /*
-    round constants:
-        the first 32 bits of the fractional parts of the cube roots of the first sixty four primes
-    */
-    var k = [64]uint32{
+/*
+  round constants:
+    the first 32 bits of the fractional parts of the cube roots of the first sixty four primes
+*/
+var k = [64]uint32{
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
     0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -33,39 +33,39 @@
     0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
     0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
-    }
+}
 
-    /*
-    the 'choose' logical operation used in sha2-256, the x input 'chooses'
-    betweeh the bits of y or z
-    */
-    func ch(x, y, z uint32) uint32 {
+/*
+  the 'choose' logical operation used in sha2-256, the x input 'chooses'
+  betweeh the bits of y or z
+*/
+func ch(x, y, z uint32) uint32 {
     return (x & y) ^ (bitCompliment32(x) & z)
-    }
+}
 
-    /*
-    the 'majority' logic operation used in sha2-256, the resulting bits
-    are the majority bits at each index from the three inputs x, y, z
-    */
-    func maj(x, y, z uint32) uint32 {
+/*
+  the 'majority' logic operation used in sha2-256, the resulting bits
+  are the majority bits at each index from the three inputs x, y, z
+*/
+func maj(x, y, z uint32) uint32 {
     return (x & y) ^ (x & z) ^ (y & z)
-    }
+}
 
-    /*
-    a (~) compliment operator for 32 bit unsigned integers used by ch
-    */
-    func bitCompliment32(x uint32) uint32 {
+/*
+  a (~) compliment operator for 32 bit unsigned integers used by ch
+*/
+func bitCompliment32(x uint32) uint32 {
     return x ^ 0xFFFFFFFF
-    }
+}
 
-    /*
-    pads the original message of length l with:
-        - a '1' bit
-        - z number of '0' bits such that l + 1 + z ≡ 448 mod 512
-        - the big endian representation of l as a 64 bit unsigned int
-    and returns the padded message
-    */
-    func padMessage(msg []byte) []byte {
+/*
+  pads the original message of length l with:
+    - a '1' bit
+    - z number of '0' bits such that l + 1 + z ≡ 448 mod 512
+    - the big endian representation of l as a 64 bit unsigned int
+  and returns the padded message
+*/
+func padMessage(msg []byte) []byte {
     l := len(msg)
     msg = append(msg, 0x80)
 
@@ -79,14 +79,14 @@
     msg = append(msg, binLen64...)
 
     return msg
-    }
+}
 
-    /*
-    packs a single 512 bit block into the first 16 words of the
-    message schedule array (w) and returns the message schedule
-    array
-    */
-    func packBlockIntoWords(block []byte) [64]uint32 {
+/*
+  packs a single 512 bit block into the first 16 words of the
+  message schedule array (w) and returns the message schedule
+  array
+*/
+func packBlockIntoWords(block []byte) [64]uint32 {
     var w [64]uint32
     offset := 0
 
@@ -96,14 +96,14 @@
     }
 
     return w
-    }
+}
 
-    /*
-    extends the 512 bit block that was packed into the first 16 words
-    of the message schedule array into the remaining 48 words and returns
-    the message schedule array
-    */
-    func extendWords(w [64]uint32) [64]uint32 {
+/*
+  extends the 512 bit block that was packed into the first 16 words
+  of the message schedule array into the remaining 48 words and returns
+  the message schedule array
+*/
+func extendWords(w [64]uint32) [64]uint32 {
     for i := 16; i < 64; i++ {
         s0 := bits.RotateLeft32(w[i-15], -7) ^ bits.RotateLeft32(w[i-15], -18) ^ (w[i-15] >> 3)
         s1 := bits.RotateLeft32(w[i-2], -17) ^ bits.RotateLeft32(w[i-2], -19) ^ (w[i-2] >> 10)
@@ -111,14 +111,14 @@
     }
 
     return w
-    }
+}
 
-    /*
-    computes the ith block's intermediate hash value using the i - 1
-    block's hash values, effectively compressing the block into
-    the hash. Returns the ith block's internediate hash values
-    */
-    func compress(w [64]uint32, hv [8]uint32) [8]uint32 {
+/*
+  computes the ith block's intermediate hash value using the i - 1
+  block's hash values, effectively compressing the block into
+  the hash. Returns the ith block's internediate hash values
+*/
+func compress(w [64]uint32, hv [8]uint32) [8]uint32 {
     a := hv[0]
     b := hv[1]
     c := hv[2]
@@ -154,14 +154,14 @@
     hv[7] += h
 
     return hv
-    }
+}
 
-    /*
-    processes a message by padding the message, packing it into the
-    message schedule array, and compressing all the blocks into the
-    hash values
-    */
-    func processMessage(msg []byte) [8]uint32 {
+/*
+  processes a message by padding the message, packing it into the
+  message schedule array, and compressing all the blocks into the
+  hash values
+*/
+func processMessage(msg []byte) [8]uint32 {
     msg = padMessage(msg)
     byteLen := len(msg)
 
@@ -175,13 +175,13 @@
     }
 
     return hashValues
-    }
+}
 
-    /*
-    client hash function, processes the message and returns the 256
-    bit digest (the hash) of the message
-    */
-    func Hash256(msg []byte) []byte {
+/*
+  client hash function, processes the message and returns the 256
+  bit digest (the hash) of the message
+*/
+func Hash256(msg []byte) []byte {
     hashValues := processMessage(msg)
     currHashValue := make([]byte, u32Bytes)
     var digest []byte
@@ -192,4 +192,4 @@
     }
 
     return digest
-    }
+}
